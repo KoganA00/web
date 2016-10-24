3105efb90e4d815a44a13908fd15ebf9ddcc35d2
Alexandra Kogan <yarolika00@mail.ru>
2016-10-24 21:21:21 +0300

    Add files via upload

---
+++ Website/inform.js
@@ -0,0 +1,92 @@
+function info(){
+		var geography_elem = document.getElementById("geography");
+		var geography1_elem = document.getElementById("geography1");
+        var politics_elem = document.getElementById("politics");
+		var politics1_elem = document.getElementById("politics1");
+		var climate_elem = document.getElementById("climate");
+		var climate1_elem = document.getElementById("climate1");
+		var animals_elem = document.getElementById("animals");
+		var animals1_elem = document.getElementById("animals1");
+        politics_elem.style.display = 'none';
+		politics1_elem.style.display = 'none';
+		animals_elem.style.display = 'none';
+		animals1_elem.style.display = 'none';
+		climate_elem.style.display = 'none';
+		climate1_elem.style.display = 'none';
+		geography_elem.style.display = 'none';
+		geography1_elem.style.display = 'none';
+}	
+window.onload=info;	
+function show_geography() {
+		var geography_elem = document.getElementById("geography");
+		var geography1_elem = document.getElementById("geography1");
+        var politics_elem = document.getElementById("politics");
+		var politics1_elem = document.getElementById("politics1");
+		var climate_elem = document.getElementById("climate");
+		var climate1_elem = document.getElementById("climate1");
+		var animals_elem = document.getElementById("animals");
+		var animals1_elem = document.getElementById("animals1");
+        politics_elem.style.display = 'none';
+		politics1_elem.style.display = 'none';
+		animals_elem.style.display = 'none';
+		animals1_elem.style.display = 'none';
+		climate_elem.style.display = 'none';
+		climate1_elem.style.display = 'none';
+		geography_elem.style.display = 'block';
+		geography1_elem.style.display = 'block';
+    }
+function show_animals() {
+		var geography_elem = document.getElementById("geography");
+		var geography1_elem = document.getElementById("geography1");
+        var politics_elem = document.getElementById("politics");
+		var politics1_elem = document.getElementById("politics1");
+		var climate_elem = document.getElementById("climate");
+		var climate1_elem = document.getElementById("climate1");
+		var animals_elem = document.getElementById("animals");
+		var animals1_elem = document.getElementById("animals1");
+        politics_elem.style.display = 'none';
+		politics1_elem.style.display = 'none';
+		animals_elem.style.display = 'block';
+		animals1_elem.style.display = 'block';
+		climate_elem.style.display = 'none';
+		climate1_elem.style.display = 'none';
+		geography_elem.style.display = 'none';
+		geography1_elem.style.display = 'none';
+    }
+function show_politics() {
+
+		var geography_elem = document.getElementById("geography");
+		var geography1_elem = document.getElementById("geography1");
+        var politics_elem = document.getElementById("politics");
+		var politics1_elem = document.getElementById("politics1");
+		var climate_elem = document.getElementById("climate");
+		var climate1_elem = document.getElementById("climate1");
+		var animals_elem = document.getElementById("animals");
+		var animals1_elem = document.getElementById("animals1");
+        politics_elem.style.display = 'block';
+		politics1_elem.style.display = 'block';
+		animals_elem.style.display = 'none';
+		animals1_elem.style.display = 'none';
+		climate_elem.style.display = 'none';
+		climate1_elem.style.display = 'none';
+		geography_elem.style.display = 'none';
+		geography1_elem.style.display = 'none';
+    }
+function show_climate() {
+		var geography_elem = document.getElementById("geography");
+		var geography1_elem = document.getElementById("geography1");
+        var politics_elem = document.getElementById("politics");
+		var politics1_elem = document.getElementById("politics1");
+		var climate_elem = document.getElementById("climate");
+		var climate1_elem = document.getElementById("climate1");
+		var animals_elem = document.getElementById("animals");
+		var animals1_elem = document.getElementById("animals1");
+        politics_elem.style.display = 'none';
+		politics1_elem.style.display = 'none';
+		animals_elem.style.display = 'none';
+		animals1_elem.style.display = 'none';
+		climate_elem.style.display = 'block';
+		climate1_elem.style.display = 'block';
+		geography_elem.style.display = 'none';
+		geography1_elem.style.display = 'none';
+    }
